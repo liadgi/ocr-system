4d94dd1f34d4e96d6a0e19b54ce05f5f241a6ec1
Liad Ginosar <liadginosar@gmail.com>
2018-04-09 13:19:53 +0300

    completing full build cycle

--- LocalApp/src/dsp/liadginosar/localapp/LocalApplication.java
+++ LocalApp/src/dsp/liadginosar/localapp/LocalApplication.java
@@ -3,8 +3,17 @@ package dsp.liadginosar.localapp;
 import java.io.IOException;
 
 public class LocalApplication {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, Exception {
+        if (args.length == 2) {
+            String filename = args[0];
+            int numOfWorkers = Integer.parseInt(args[1]);
 
+            System.out.println("Filename: " + filename);
+            System.out.println("workers #:" + numOfWorkers);
+
+        } else {
+            throw new Exception("Incorrect number of arguments.");
+        }
 
     }
 }
