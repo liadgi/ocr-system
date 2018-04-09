77c10e9ee734fd8e07f9fba79eba5acc301be2b6
Liad Ginosar <liadginosar@gmail.com>
2018-04-09 19:07:07 +0300

    after installing aws cli, setting credentials, creating ec2 instance

--- LocalApp/src/dsp/liadginosar/localapp/LocalApplication.java
+++ LocalApp/src/dsp/liadginosar/localapp/LocalApplication.java
@@ -1,5 +1,12 @@
 package dsp.liadginosar.localapp;
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.InstanceType;
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+import com.amazonaws.services.ec2.model.RunInstancesResult;
+
 import java.io.IOException;
 
 public class LocalApplication {
@@ -10,6 +17,26 @@ public class LocalApplication {
 
             System.out.println("Filename: " + filename);
             System.out.println("workers #:" + numOfWorkers);
+
+            RunInstancesRequest runInstancesRequest =
+                    new RunInstancesRequest();
+
+            runInstancesRequest.withImageId("ami-1853ac65")
+                    .withInstanceType(InstanceType.T2Micro)
+                    .withMinCount(1)
+                    .withMaxCount(1)
+                    .withKeyName("dist-sys-course")
+                    .withSecurityGroups("launch-wizard-1");
+
+            AmazonEC2 amazonEC2Client =
+            AmazonEC2ClientBuilder.standard()
+                    .withCredentials(new DefaultAWSCredentialsProviderChain())
+                    .build();
+
+            RunInstancesResult result = amazonEC2Client.runInstances(
+                    runInstancesRequest);
+
+            System.out.println("Done.");
 
         } else {
             throw new Exception("Incorrect number of arguments.");
--- Manager/src/dsp/liadginosar/manager/Manager.java
+++ Manager/src/dsp/liadginosar/manager/Manager.java
@@ -1,11 +1,13 @@
 package dsp.liadginosar.manager;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class Manager {
 
-    public static void main(String[] args) throws IOException {
-
-
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Manager started, listening on SQS:");
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println("Manager stopped.");
     }
 }
