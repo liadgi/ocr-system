983587158db58d2f4d427383ce9ae1f1dadc9719
Liad Ginosar <liadginosar@gmail.com>
2018-04-09 12:50:57 +0300

    setting up manager and local app modules

---
+++ LocalApp/src/dsp/liadginosar/localapp/LocalApplication.java
@@ -0,0 +1,10 @@
+package dsp.liadginosar.localapp;
+
+import java.io.IOException;
+
+public class LocalApplication {
+    public static void main(String[] args) throws IOException {
+
+
+    }
+}
---
+++ Manager/src/dsp/liadginosar/manager/Manager.java
@@ -0,0 +1,11 @@
+package dsp.liadginosar.manager;
+
+import java.io.IOException;
+
+public class Manager {
+
+    public static void main(String[] args) throws IOException {
+
+
+    }
+}
