a2d4c8d7770c75fe83b81bbf04559055699089b5
Liad Ginosar <liadginosar@gmail.com>
2018-04-11 19:19:14 +0300

    progress on worker and local app

--- LocalApp/src/dsp/liadginosar/localapp/LocalApplication.java
+++ LocalApp/src/dsp/liadginosar/localapp/LocalApplication.java
@@ -9,17 +9,30 @@ import com.amazonaws.services.ec2.model.RunInstancesRequest;
 import com.amazonaws.services.ec2.model.RunInstancesResult;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.*;
 
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Base64;
+import java.util.Date;
+import java.util.List;
 
 public class LocalApplication {
+
+    private static final String QUEUE_APP_TO_MANAGER = "LocalAppToManagerQueue" + new Date().getTime();
+    private AmazonSQS sqs;
+    private String queueUrl;
+
+    private String getKeyName(String file_path) {
+        return Paths.get(file_path).getFileName().toString();
+    }
 
     private void uploadImagesLinksFile(String file_path) {
         final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
         try {
-            String key_name = Paths.get(file_path).getFileName().toString();
+            String key_name = getKeyName(file_path);
             s3.putObject("dsp-ocr", key_name, file_path);
         } catch (AmazonServiceException e) {
             System.err.println(e.getErrorMessage());
@@ -27,11 +40,27 @@ public class LocalApplication {
         }
     }
 
-    private void updateQueue(){
+    private void initQueue() {
+        sqs = AmazonSQSClientBuilder.defaultClient();
+        try {
+            CreateQueueResult create_result = sqs.createQueue(QUEUE_APP_TO_MANAGER);
+        } catch (AmazonSQSException e) {
+            if (!e.getErrorCode().equals("QueueAlreadyExists")) {
+                throw e;
+            }
+        }
 
+        queueUrl = sqs.getQueueUrl(QUEUE_APP_TO_MANAGER).getQueueUrl();
+
+        // Enable long polling on an existing queue
+        SetQueueAttributesRequest set_attrs_request = new SetQueueAttributesRequest()
+                .withQueueUrl(queueUrl)
+                .addAttributesEntry("ReceiveMessageWaitTimeSeconds", "20");
+        sqs.setQueueAttributes(set_attrs_request);
     }
 
-    private void startManager(int numOfWorkers) {
+
+    private void initManagerInstance(int numOfWorkers) {
         String userDataInitScript = "#!/bin/bash\nwget https://s3.amazonaws.com/dsp-ocr/Manager.jar -O ~/Manager.jar\njava -jar ~/Manager.jar " + numOfWorkers;
 
         byte[] encodedBytes = Base64.getEncoder().encode(userDataInitScript.getBytes());
@@ -58,6 +87,14 @@ public class LocalApplication {
                 runInstancesRequest);
     }
 
+    private void notifyManagerDownloadImagesFile(String file_path) {
+        SendMessageRequest send_msg_request = new SendMessageRequest()
+                .withQueueUrl(queueUrl)
+                .withMessageBody("new task " + getKeyName(file_path))
+                .withDelaySeconds(5);
+        sqs.sendMessage(send_msg_request);
+    }
+
     public static void main(String[] args) throws IOException, Exception {
 
         if (args.length == 2) {
@@ -71,14 +108,40 @@ public class LocalApplication {
             LocalApplication app = new LocalApplication();
 
             app.uploadImagesLinksFile(file_path);
-            app.updateQueue();
-            app.startManager(numOfWorkers);
+            app.initQueue();
+            app.initManagerInstance(numOfWorkers);
+            app.notifyManagerDownloadImagesFile(file_path);
+
+            String s3FileLocation = app.retrieveOutputFileLocation();
+            app.WriteFileToDisk(s3FileLocation);
 
             System.out.println("Done.");
 
         } else {
             throw new Exception("Incorrect number of arguments.");
         }
+
+    }
+
+    private String retrieveOutputFileLocation() {
+        String s3FileLocation = null;
+        // Enable long polling on a message receipt
+        ReceiveMessageRequest receive_request = new ReceiveMessageRequest()
+                .withQueueUrl(queueUrl)
+                .withWaitTimeSeconds(20);
+        List<Message> messages = sqs.receiveMessage(receive_request).getMessages();
+
+        for (Message m : messages) {
+            if (m.getBody().startsWith("done task")) {
+                String[] arr = m.getBody().split("done task");
+                s3FileLocation = arr[1];
+                sqs.deleteMessage(queueUrl, m.getReceiptHandle());
+            }
+        }
+        return s3FileLocation;
+    }
+
+    private void WriteFileToDisk(String s3FileLocation) {
 
     }
 }
--- Manager/src/dsp/liadginosar/manager/Manager.java
+++ Manager/src/dsp/liadginosar/manager/Manager.java
@@ -1,15 +1,108 @@
 package dsp.liadginosar.manager;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.util.List;
 
 public class Manager {
+    private static final String QUEUE_APP_TO_MANAGER = "LocalAppToManagerQueue" + new Date().getTime();
+    private AmazonSQS sqs;
+    private String queueUrl;
+
+    private void initReceivingQueue() {
+        sqs = AmazonSQSClientBuilder.defaultClient();
+        try {
+            CreateQueueResult create_result = sqs.createQueue(QUEUE_APP_TO_MANAGER);
+        } catch (AmazonSQSException e) {
+            if (!e.getErrorCode().equals("QueueAlreadyExists")) {
+                throw e;
+            }
+        }
+
+        queueUrl = sqs.getQueueUrl(QUEUE_APP_TO_MANAGER).getQueueUrl();
+
+        // Enable long polling on an existing queue
+        SetQueueAttributesRequest set_attrs_request = new SetQueueAttributesRequest()
+                .withQueueUrl(queueUrl)
+                .addAttributesEntry("ReceiveMessageWaitTimeSeconds", "20");
+        sqs.setQueueAttributes(set_attrs_request);
+    }
+
+    private List<Message> retreiveMessagesFromQueue() {
+        // Enable long polling on a message receipt
+        ReceiveMessageRequest receive_request = new ReceiveMessageRequest()
+                .withQueueUrl(queueUrl)
+                .withWaitTimeSeconds(20);
+        List<Message> messages = sqs.receiveMessage(receive_request).getMessages();
+
+        return messages;
+    }
+
+    private void downloadInputFile(String key_name) {
+        final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+        try {
+            S3Object o = s3.getObject("dsp-ocr", key_name);
+            S3ObjectInputStream s3is = o.getObjectContent();
+            FileOutputStream fos = new FileOutputStream(new File(key_name));
+            byte[] read_buf = new byte[1024];
+            int read_len = 0;
+            while ((read_len = s3is.read(read_buf)) > 0) {
+                fos.write(read_buf, 0, read_len);
+            }
+            s3is.close();
+            fos.close();
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+    }
 
     public static void main(String[] args) throws IOException, InterruptedException {
         int numOfWorkers = Integer.parseInt(args[0]);
+        Manager manager = new Manager();
         System.out.println("Manager started, # of workers: " + numOfWorkers);
+
         System.out.println("started listening on SQS:");
-        TimeUnit.SECONDS.sleep(5);
+        manager.initReceivingQueue();
+        List<Message> messages = manager.retreiveMessagesFromQueue();
+        String inputFileLocation = manager.retrieveInputFileLocation(messages);
+
+        System.out.println("Download input file from S3:");
+        manager.downloadInputFile(inputFileLocation);
+        // read file and map urls to workers
+
+        
+
         System.out.println("Manager stopped.");
+    }
+
+    private String retrieveInputFileLocation(List<Message> messages) {
+        String fileLocation = null;
+        for (Message m : messages) {
+            if (m.getBody().startsWith("new task")) {
+                String[] arr = m.getBody().split("new task");
+                fileLocation = arr[1];
+                sqs.deleteMessage(queueUrl, m.getReceiptHandle());
+            }
+        }
+        return fileLocation;
     }
 }
